a3ffcb670e109e08dfbb8a4e7983a9b718ea04c0
Tom <tombjh@hotmail.co.uk>
2018-10-09 20:12:34 +0100

    added testing for note

--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -1,4 +1,23 @@
 require 'rspec'
 require_relative '../lib/note'
 
+describe Note do
+  subject(:note){described_class.new('test note title', 'test note body')}
+  describe '#new' do
+    it 'should create a new note object' do
+      expect(described_class).to eq(Note)
+    end
+  end
+  describe 'title' do
+    it 'should return the tile of the note' do
+      expect(note.title).to eq('test note title')
+    end
+  end
+  describe 'body' do
+    it 'should return the body of the note' do
+      expect(note.body).to eq('test note body')
+    end
+  end
+end
+
 
