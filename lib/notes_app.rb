973dc632663993128403a765727321caa44b9a42
Tom Hird <tombjh@hotmail.co.uk>
2018-10-09 14:08:19 +0000

    added reminder prompt to note_app.rb

--- lib/notes_app.rb
+++ lib/notes_app.rb
@@ -2,8 +2,17 @@ class Notes_app
   def initialize
     @stored_notes = Array.new
   end
+  
 def add_note(title, body)
-  not = Not.new(title, body)
+  note = Not.new(title, body)
   @stored_notes.push(note)
+end
+
+def create_note_prompt
+  puts 'Title of the note:'
+  title = gets.chomp
+  puts 'And the body?'
+  body = gets.chomp
+  add_note(title, body)
 end
 end
