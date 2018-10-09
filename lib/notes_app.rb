342eb084b822ad642ce2fa3bf391148f8fa07e75
Tom Hird <tombjh@hotmail.co.uk>
2018-10-09 14:28:38 +0000

    added reminder prompt to note_app.rb

--- lib/notes_app.rb
+++ lib/notes_app.rb
@@ -15,4 +15,15 @@ def create_note_prompt
   body = gets.chomp
   add_note(title, body)
 end
+
+def list_all
+  @stored_notes.each do |note|
+    puts note.title
+  end
+
+def choose_note(target_title)
+  note = (@stored_notes.select { |a note| a_note.title == target_title })[0]
+  puts note.title
+  puts note.body
+  end
 end
